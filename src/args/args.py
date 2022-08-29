10bc8cfcf4c02bd0ac7ea42a44adfe7aa344d333
dugonzal <ciclocontinuo2@gmail.com>
2022-08-29 22:39:29 +0200

    upload

--- src/args/args.py
+++ src/args/args.py
@@ -1,7 +1,7 @@
 import argparse
 
 def ft_args():
-	parcel = argparse.ArgumentParser(description='stockholm es un ransomware', \
+	parcel = argparse.ArgumentParser(description='stockholm es un ransomware que encripta y desencripta todos los archivos de la carpeta ~/infection', \
 	epilog='version 1.0.0, author: dugonzal | Haz el mal con fines educativos.')
 	parcel.add_argument('-v', '--version', action='version', version='%(prog)s | version: 1.0.0')
 	parcel.add_argument('-r', '--reverse', action='store_true', help='descrypt the files')
